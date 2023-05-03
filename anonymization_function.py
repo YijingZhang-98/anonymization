c6cbe70c7f7d49328b57084c89f45aa3cfdb145c
YijingZhang-98 <yijingzhang98@gmail.com>
2023-05-02 22:44:59 -0500

    upload anonymization code

---
+++ anonymization_function.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import hashlib
+import uuid
+
+# Import data
+df = pd.read_csv('data.csv')
+anonymized_df = df.copy()
+
+# Function to generate a random UUID
+def generate_uuid():
+    return str(uuid.uuid4())
+anonymized_df['Name'] = anonymized_df['Name'].apply(lambda x: generate_uuid())
+
+# Function to hash a phone number using SHA256
+def hash_phone_number(phone_number):
+    return hashlib.sha256(phone_number.encode('utf-8')).hexdigest()
+anonymized_df['PhoneNumber'] = anonymized_df['PhoneNumber'].apply(hash_phone_number)
+
+# Function to hash an address
+def hash_address(address):
+    return hashlib.sha256(address.encode()).hexdigest()
+anonymized_df['Address'] = anonymized_df['Address'].apply(lambda x: hash_address(x))
